38c9c123fbd28401386178e46fe51980acd1322c
4er7il0 <TEDragon24@gmail.com>
2022-11-10 14:00:23 +0500

    first commit

---
+++ app/src/main/java/com/example/jetpackcompose_kunavin_pr33/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.jetpackcompose_kunavin_pr33.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Red = Color(0xFFef6054)
+val Green = Color(0xFF60D394)
+val LightGreen = Color(0xFFabf684)
+val Yellow = Color(0xFFffda7e)
+val LightRed = Color(0xFFff9c86)
+val LightGray = Color(0xFFECECEC)
